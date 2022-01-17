339f7403d48c0112e3b32481b2f02e60da03ae00
Ssujimu <okq0223@naver.com>
2022-01-17 16:42:43 +0900

    ExProblem

---
+++ FirstPrj/src/ExProblem/Ex1.java
@@ -0,0 +1,11 @@
+package ExProblem;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("첫 Java 프로그램입니다");
+		System.out.println("화면에 출력하고 있습니다.");
+	}
+
+}
---
+++ FirstPrj/src/ExProblem/Ex2.java
@@ -0,0 +1,13 @@
+package ExProblem;
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("¹Ú");
+		System.out.println();
+		System.out.println("¼ö");
+		System.out.println("Áö");
+	}
+
+}
