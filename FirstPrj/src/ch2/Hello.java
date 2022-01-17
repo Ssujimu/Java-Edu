ea0eb83ee5a7618ade1075179c1c225f748b3c7b
Ssujimu <okq0223@naver.com>
2022-01-17 16:42:56 +0900

    FirstJava

---
+++ FirstPrj/src/ch2/Hello.java
@@ -0,0 +1,10 @@
+package ch2;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("주소 : 서울시 송파구 오금로20길 7 202호");
+	}
+
+}
