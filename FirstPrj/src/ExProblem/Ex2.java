d7e29f9163dc39ca7017e78e5488c522c7c77fbf
Ssujimu <90470172+Ssujimu@users.noreply.github.com>
2022-01-17 18:28:27 +0900

    Update Ex2.java

--- FirstPrj/src/ExProblem/Ex2.java
+++ FirstPrj/src/ExProblem/Ex2.java
@@ -4,10 +4,10 @@ public class Ex2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("¹Ú");
+		System.out.println("ë°•");
 		System.out.println();
-		System.out.println("¼ö");
-		System.out.println("Áö");
+		System.out.println("ìˆ˜");
+		System.out.println("ì§€");
 	}
 
 }
