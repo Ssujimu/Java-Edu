9bb67f928d2c6bc0382642ceec13e6f77c2d8127
Ssujimu <90470172+Ssujimu@users.noreply.github.com>
2022-01-18 12:50:08 +0900

    SumAvDif

---
+++ FirstPrj/src/ExProblem/Ex3SumDiff.java
@@ -0,0 +1,12 @@
+package ExProblem;
+
+public class Ex3SumDiff {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 82, b = 17;
+		System.out.println("82와 17의 합 : " + (a + b));
+		System.out.println("82와 17의 차 : " + (a - b));
+	}
+
+}
---
+++ FirstPrj/src/ExProblem/Ex4SumAve.java
@@ -0,0 +1,12 @@
+package ExProblem;
+
+public class Ex4SumAve {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 63, y = 18;
+		System.out.println("변수 두 개의 합 : " + (x + y));
+		System.out.println("변수 두 개의 평균 : " + (x + y)/2);
+	}
+
+}
