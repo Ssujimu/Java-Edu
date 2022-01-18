da23eb48003f10173a404dd625b8a0bf1653218c
Ssujimu <90470172+Ssujimu@users.noreply.github.com>
2022-01-18 16:55:46 +0900

    I can use Scanner

---
+++ FirstPrj/src/ExProblem/Ex6Scanner.java
@@ -0,0 +1,16 @@
+package ExProblem;
+
+import java.util.Scanner;
+
+public class Ex6Scanner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("값을 입력하시오.");
+		a = sc.nextInt();
+		System.out.println(a);
+	}
+
+}
---
+++ FirstPrj/src/ExProblem/Ex7ScanAddre.java
@@ -0,0 +1,16 @@
+package ExProblem;
+
+import java.util.Scanner;
+
+public class Ex7ScanAddre {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String Add;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("주소를 입력하세요.");
+		Add = sc.nextLine();
+		System.out.println(Add);
+	}
+
+}
