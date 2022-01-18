09a172d69fa1862e4de1c8150499affc57eccf03
Ssujimu <90470172+Ssujimu@users.noreply.github.com>
2022-01-18 16:05:25 +0900

    Update MyCuri.java

--- FirstPrj/src/ExProblem/MyCuri.java
+++ FirstPrj/src/ExProblem/MyCuri.java
@@ -4,6 +4,7 @@ public class MyCuri {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// 후위증가 전위감소 
 		int a = 10;
 		System.out.println(a++);
 		System.out.println(--a);
