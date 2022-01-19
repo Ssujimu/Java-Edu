21501bf18329b44ce7e3323afd34e0ed504d9caf
Ssujimu <90470172+Ssujimu@users.noreply.github.com>
2022-01-19 12:06:36 +0900

    3OpEx
    my score

---
+++ FirstPrj/src/ch3/ConditionalOperationEx.java
@@ -0,0 +1,12 @@
+package ch3;
+
+public class ConditionalOperationEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int score = 95;
+		char grade = (score > 90) ? 'A' : ((score > 80) ? 'B' : 'C');
+		System.out.println(score + "점이고 " + grade + " 학점 입니다.");
+	}
+
+}
