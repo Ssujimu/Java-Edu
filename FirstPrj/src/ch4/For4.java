521c44d028410a609f276688578cfbd68cb692d2
Ssujimu <90470172+Ssujimu@users.noreply.github.com>
2022-01-19 15:11:01 +0900

    Forgugu3dan

---
+++ FirstPrj/src/ch4/For4.java
@@ -0,0 +1,16 @@
+package ch4;
+
+public class For4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		System.out.println("구구단 3단 출력");
+		
+		int dan = 3;
+		for(i =1; i < 10; i++) {
+			System.out.println(dan + " * " + i + " = " + (dan*i));
+		}
+	}
+
+}
