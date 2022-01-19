d8a2c78b93ce324297a37e4b071a6af24e9c6060
Ssujimu <90470172+Ssujimu@users.noreply.github.com>
2022-01-19 12:29:32 +0900

    Ex7ScanAddre

--- FirstPrj/src/ExProblem/Ex7ScanAddre.java
+++ FirstPrj/src/ExProblem/Ex7ScanAddre.java
@@ -6,7 +6,7 @@ public class Ex7ScanAddre {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String Add;
+		String Add;					// String은 객체여
 		Scanner sc = new Scanner(System.in);
 		System.out.println("주소를 입력하세요.");
 		Add = sc.nextLine();
