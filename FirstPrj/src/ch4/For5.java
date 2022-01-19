793c01fafb6f9cd51bfcbd1356adffae4cd707c3
Ssujimu <90470172+Ssujimu@users.noreply.github.com>
2022-01-19 15:42:23 +0900

    ForGugudan

---
+++ FirstPrj/src/ch4/For5.java
@@ -0,0 +1,17 @@
+package ch4;
+
+public class For5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i, j;
+		for(i=2; i<10; i++) {										// 단
+			for(j=1; j<10; j++) {									// 곱해지는 수
+				System.out.println(i+ " * " + j + " = " + (i*j));
+			}
+			System.out.println();
+		}
+	}
+
+}
+                                              
