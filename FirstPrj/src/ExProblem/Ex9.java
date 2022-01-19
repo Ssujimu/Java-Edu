c3b79827cb3e12b08917a322cd706bc0c4254171
Ssujimu <90470172+Ssujimu@users.noreply.github.com>
2022-01-19 15:22:39 +0900

    Ex9SwitchAllSeason

---
+++ FirstPrj/src/ExProblem/Ex9.java
@@ -0,0 +1,56 @@
+package ExProblem;
+
+import java.util.Scanner;
+
+public class Ex9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 0;
+		
+		System.out.println("월을 입력하세요.");
+		Scanner sc = new Scanner(System.in);
+		a= sc.nextInt();
+		
+		switch (a) {
+		case 1:
+			System.out.println("겨울");
+			break;
+		case 2:
+			System.out.println("겨울");
+			break;
+		case 3:
+			System.out.println("봄");
+			break;
+		case 4:
+			System.out.println("봄");
+			break;
+		case 5:
+			System.out.println("봄");
+			break;
+		case 6:
+			System.out.println("여름");
+			break;
+		case 7:
+			System.out.println("여름");
+			break;
+		case 8:
+			System.out.println("여름");
+			break;
+		case 9:
+			System.out.println("가을");
+			break;
+		case 10:
+			System.out.println("가을");
+			break;
+		case 11:
+			System.out.println("가을");
+			break;
+		case 12:
+			System.out.println("겨울");
+			break;
+		}
+		
+	}
+
+}
