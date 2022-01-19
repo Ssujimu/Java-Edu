a3249955e7b43fce807c67b49212ab0132498d9b
Ssujimu <90470172+Ssujimu@users.noreply.github.com>
2022-01-19 12:53:40 +0900

    Update Ex3SumDiff.java

--- FirstPrj/src/ExProblem/Ex3SumDiff.java
+++ FirstPrj/src/ExProblem/Ex3SumDiff.java
@@ -5,8 +5,8 @@ public class Ex3SumDiff {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a = 82, b = 17;
-		System.out.println("82와 17의 합 : " + (a + b));
-		System.out.println("82와 17의 차 : " + (a - b));
+		System.out.println("82와 17의 합 : " + (a + b));		// 82와 17의 합 : 92
+		System.out.println("82와 17의 차 : " + (a - b));		// 82와 17의 차 : 65
 	}
 
 }
