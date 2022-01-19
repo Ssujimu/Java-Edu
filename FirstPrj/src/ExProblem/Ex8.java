43acd5268add874e9d169b599c8843e1f198c010
Ssujimu <90470172+Ssujimu@users.noreply.github.com>
2022-01-19 13:06:39 +0900

    ScanIf

---
+++ FirstPrj/src/ExProblem/Ex8.java
@@ -0,0 +1,26 @@
+package ExProblem;
+
+import java.util.Scanner;
+
+public class Ex8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 정숫값을 읽어 부호(양수/음수/0)를 판정하는 프로그램을 작성
+		
+		int a;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("정수를 입력하세요.");
+		a=sc.nextInt();
+		
+		if (a > 0) {
+			System.out.println("양수 입니다.");
+		} else if (a < 0){
+			System.out.println("음수 입니다.");
+		} else if (a==0) {
+			System.out.println("0 입니다.");
+		}
+	}
+
+}
