6cf5733907e9bdee6f89daa0c98043115f083ac8
Ssujimu <90470172+Ssujimu@users.noreply.github.com>
2022-01-19 12:08:01 +0900

    Op4

---
+++ FirstPrj/src/ch3/Op4.java
@@ -0,0 +1,20 @@
+package ch3;
+
+public class Op4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10, b = 20;
+		
+		System.out.println(a==10 && b==20);		// true
+		System.out.println(a>10 && b==20);		// false
+		System.out.println(a==10 && b==10);		// false
+		System.out.println(a<10 && b>20);		// false
+		
+		System.out.println(a==10 || b==20);		// true
+		System.out.println(a>20 || b==20);		// true
+		System.out.println(a==10 || b==10);		// true
+		System.out.println(a<10 || b>20);		// false
+	}
+
+}
