54e9dfadb0e3cf645984d7e0365495dd72fa1758
Ssujimu <90470172+Ssujimu@users.noreply.github.com>
2022-01-21 19:35:44 +0900

    Update Ex1.java

--- FirstPrj/src/ExProblem/Ex1.java
+++ FirstPrj/src/ExProblem/Ex1.java
@@ -3,7 +3,7 @@ package ExProblem;
 public class Ex1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 화면에 "첫 Java 프로그램 입니다"와 "화면에 출력하고 있습니다."를 한 줄씩 출력 하시오.
 		System.out.println("첫 Java 프로그램입니다");		// 첫 Java 프로그램입니다
 		System.out.println("화면에 출력하고 있습니다.");		// 화면에 출력하고 있습니다.
 	}
