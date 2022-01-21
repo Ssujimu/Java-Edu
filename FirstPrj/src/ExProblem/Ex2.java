ca597c4afb7cd3edf5cb4638342ddff47a52e564
Ssujimu <90470172+Ssujimu@users.noreply.github.com>
2022-01-21 19:37:00 +0900

    Update Ex2.java

--- FirstPrj/src/ExProblem/Ex2.java
+++ FirstPrj/src/ExProblem/Ex2.java
@@ -3,7 +3,7 @@ package ExProblem;
 public class Ex2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 자기 이름 한 줄 씩 출력하기. 대신, 성 다음 한 줄 띄고 출력하기
 		System.out.println("박");
 		System.out.println();
 		System.out.println("수");
