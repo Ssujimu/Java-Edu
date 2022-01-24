ffa3cacfc0c1b74b9a5a04c7912fcc17096cd9c0
Ssujimu <90470172+Ssujimu@users.noreply.github.com>
2022-01-24 11:31:06 +0900

    Shape

---
+++ FirstPrj/src/ch7/ShapeMain.java
@@ -0,0 +1,13 @@
+package ch7;
+
+public class ShapeMain {
+
+	public static void main(String[] args) {
+		Shape s = new Shape();	// shape 클래스 객체 s를 생성.
+		s.p.x =  10;
+		s.p.y = 20;
+		s.draw();
+
+	}
+
+}
